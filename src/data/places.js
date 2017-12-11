d7b05d72e931f76f5c7b088d1077a1caefd09012
ptya <petr.szab@gmail.com>
2017-12-11 20:44:50 +0100

    Created external Place model

---
+++ src/data/places.js
@@ -0,0 +1,28 @@
+const places = [
+    {
+        title: "Városmajor",
+        position: {lat: 47.507904, lng: 19.017603}
+    },
+    {
+        title: "CoolTour",
+        position: {lat: 47.5098299, lng: 19.0244666}
+    },
+    {
+        title: "Mátyás Templom",
+        position: {lat: 47.5019537, lng: 19.0341617}
+    },
+    {
+        title: "Margitszigeti szökőkút",
+        position: {lat: 47.5187803, lng: 19.0447954}
+    },
+    {
+        title: "Hold utcai Vásárcsarnok",
+        position: {lat: 47.504828, lng: 19.0526494}
+    },
+    {
+        title: "Pontoon",
+        position: {lat: 47.4996289, lng: 19.046212}
+    }
+];
+
+module.exports = places;
---
+++ src/models/Place.js
@@ -0,0 +1,6 @@
+const Place = function (data) {
+    this.title = data.title;
+    this.position = data.position;
+};
+
+module.exports = Place;
