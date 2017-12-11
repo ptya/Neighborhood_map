c65c710589224658e1b0c030e1fef8d40c2fa751
Szabo Peter <p.szabo@mito.hu>
2017-12-11 15:03:36 +0100

    Created Place model

---
+++ src/models/Place.js
@@ -0,0 +1,10 @@
+const Place = function(data) {
+    this.title = data.title;
+    this.lat = data.lat;
+    this.lng = data.lng;
+    /*
+    state for active?
+    */
+};
+
+module.exports = Place;
