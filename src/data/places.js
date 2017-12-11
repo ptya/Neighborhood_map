623fb763810459c3f25a39194c072753c20c6ff6
Szabo Peter <p.szabo@mito.hu>
2017-12-11 15:03:58 +0100

    Refactored markers data into places

---
+++ src/data/places.js
@@ -0,0 +1,34 @@
+const markers = [
+    {
+        title: "itt",
+        lat: 47.497667,
+        lng: 19.04103
+    },
+    {
+        title: "ott",
+        lat: 42.497667,
+        lng: 18.04103
+    },
+    {
+        title: "emitt",
+        lat: 49.497667,
+        lng: 20.04103
+    },
+    {
+        title: "amott",
+        lat: 51.497667,
+        lng: 21.04103
+    },
+    {
+        title: "emerre",
+        lat: 38.497667,
+        lng: 17.04103
+    },
+    {
+        title: "amarra",
+        lat: 40.497667,
+        lng: 16.04103
+    }
+];
+
+module.exports = markers;
