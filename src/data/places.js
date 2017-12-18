3dd5106183fa853a355efc1086aa20a153e5e0e9
Szabo Peter <p.szabo@mito.hu>
2017-12-18 14:27:39 +0100

    Updated with foursquare ids

--- src/data/places.js
+++ src/data/places.js
@@ -2,32 +2,38 @@ const places = [
     {
         title: "Városmajor",
         position: {lat: 47.5078037, lng: 19.017604699999993},
-        place_id: "ChIJNYMnvrzeQUcRSwlMyQRFsuA"
+        place_id: "ChIJNYMnvrzeQUcRSwlMyQRFsuA",
+        fsq_id: "4bbdf77fa0a0c9b6813b1c0f"
     },
     {
         title: "Cooltour Bar III.",
         position: {lat: 47.50986779999999, lng: 19.02450190000002},
-        place_id: "ChIJSQFT26beQUcRScnVLv-m26U"
+        place_id: "ChIJSQFT26beQUcRScnVLv-m26U",
+        fsq_id: "51aa67da498ef9ce6a081be2"
     },
     {
         title: "Matthias Church",
         position: {lat: 47.5019537, lng: 19.034161700000027},
-        place_id: "ChIJ1yuefiLcQUcR1Uppb4xy0GI"
+        place_id: "ChIJ1yuefiLcQUcR1Uppb4xy0GI",
+        fsq_id: "4b95fe79f964a5208eb934e3"
     },
     {
         title: "Margaret Island Fountain",
         position: {lat: 47.5187803, lng: 19.044795399999998},
-        place_id: "ChIJO4BOFgfcQUcRwlgwK3OK1Fk"
+        place_id: "ChIJO4BOFgfcQUcRwlgwK3OK1Fk",
+        fsq_id: "4db43dc1fa8c350240e05a26"
     },
     {
         title: "Hold Street Market and Downtown Market",
         position: {lat: 47.50482799999999, lng: 19.052649400000064},
-        place_id: "ChIJc7gZbhPcQUcRjPopkeIJEhA"
+        place_id: "ChIJc7gZbhPcQUcRjPopkeIJEhA",
+        fsq_id: "4bd02be477b29c74233a8a82"
     },
     {
         title: "PONTOON",
         position: {lat: 47.4996289, lng: 19.04621199999997},
-        place_id: "ChIJXXOqGD7cQUcRLF8-QKqmVDM"
+        place_id: "ChIJXXOqGD7cQUcRLF8-QKqmVDM",
+        fsq_id: "59064651446ea60929669886"
     }
 ];
 
